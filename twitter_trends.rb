c5cc8a0e878641ab1f7536da45dd7b4fc090986b
t-p <thomas@ibpfeiffer.de>
2011-03-21 15:04:06 +0100

    changed def to helper method

--- twitter_trends.rb
+++ twitter_trends.rb
@@ -3,10 +3,11 @@ require 'twitter'
 require 'sinatra'
 require 'haml'
 
-before do
-  @trend = Twitter.local_trends(638242)
-end
 
 get '/' do
   haml :index
+end
+
+def trend
+  Twitter.local_trends(638242)
 end
