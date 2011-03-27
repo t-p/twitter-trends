cadf9d6f6a755cd950698e645f339e8c8b488377
t-p <thomas@ibpfeiffer.de>
2011-03-27 11:35:36 +0200

    added links to view

--- twitter_trends.rb
+++ twitter_trends.rb
@@ -8,6 +8,15 @@ get '/' do
   haml :index
 end
 
+get '/:page' do
+  @page = Twitter::Search.new.q(params[:page]).fetch
+  haml :show
+end
+
 def trend
   Twitter.local_trends(23424829)
+end
+
+def find_trend(trend)
+  
 end
