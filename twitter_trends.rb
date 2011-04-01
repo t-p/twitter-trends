09f88a2743f035594c4f2ced6c5791a0e880d8e2
t-p <thomas@ibpfeiffer.de>
2011-04-01 10:54:59 +0200

    added pic for ext link

--- twitter_trends.rb
+++ twitter_trends.rb
@@ -14,7 +14,7 @@ helpers do
   def parse_for_url(text)
     # The regex could probably still be improved, but this seems to do the
     # trick for most cases.
-    text.gsub(/(https?:\/\/\w+(\.\w+)+(\/[\w\+\-\,\%]+)*(\?[\w\[\]]+(=\w*)?(&\w+(=\w*)?)*)?(#\w+)?)/i, '<a href="\1" target="_blank">\1</a>')
+    text.gsub(/(https?:\/\/\w+(\.\w+)+(\/[\w\+\-\,\%]+)*(\?[\w\[\]]+(=\w*)?(&\w+(=\w*)?)*)?(#\w+)?)/i, '<a class="external" href="\1" target="_blank">\1</a>')
   end
 end
 
